b203b041d8f12bd43ac1ebd7b1d945a14b813c2a
Andrew Yaroshevych <ayaroshlvua@gmail.com>
2023-05-05 15:24:00 +0200

    Install react-query

---
+++ src/api/weatherApi.js
@@ -0,0 +1,3 @@
+export const weatherApi = {
+  searchCity(query) {}
+};
--- src/index.js
+++ src/index.js
@@ -5,13 +5,17 @@ import "./index.css";
 import { router } from "./routing";
 import { RouterProvider } from "react-router-dom";
 import { theme } from "./chakra/theme";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <ChakraProvider theme={theme}>
-      <RouterProvider router={router}></RouterProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router}></RouterProvider>
+      </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
