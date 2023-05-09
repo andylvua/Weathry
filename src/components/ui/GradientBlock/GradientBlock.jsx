b7e7c44f9b2dd754b4d376993d2c236765d659e8
Andrew Yaroshevych <ayaroshlvua@gmail.com>
2023-05-09 15:46:50 +0200

    Delete color mode gradient

--- src/components/ui/GradientBlock/GradientBlock.jsx
+++ src/components/ui/GradientBlock/GradientBlock.jsx
@@ -1,27 +1,25 @@
 import styles from "./GradientedBlock.module.css";
-import { Box, useColorMode } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 
 const GradientBlock = ({ children, withoutPaddings, ...rest }) => {
-  const { colorMode } = useColorMode();
   return (
     <Box position={"relative"} borderRadius={20} p={!withoutPaddings ? 8 : 0} {...rest}>
       <Box position={"relative"} zIndex={10}>
         {children}
       </Box>
-      {colorMode === "dark" && (
-        <Box
-          borderRadius={20}
-          position={"absolute"}
-          w={"full"}
-          h={"full"}
-          top={0}
-          left={0}
-          opacity={0.2}
-          className={styles.canvas}
-        >
-          <div className={styles.circles}></div>
-        </Box>
-      )}
+
+      <Box
+        borderRadius={20}
+        position={"absolute"}
+        w={"full"}
+        h={"full"}
+        top={0}
+        left={0}
+        opacity={0.2}
+        className={styles.canvas}
+      >
+        <div className={styles.circles}></div>
+      </Box>
     </Box>
   );
 };
