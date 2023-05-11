f4a7549179fd4b2ff11f1d3020fcaeb9b0da6980
Andrew Yaroshevych <ayaroshlvua@gmail.com>
2023-05-11 12:57:22 +0200

    Redesign feedback form

--- src/components/screens/FeedbackPage/FeedbackPage.jsx
+++ src/components/screens/FeedbackPage/FeedbackPage.jsx
@@ -16,6 +16,7 @@ import {
 } from "@chakra-ui/react";
 import { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
+import GradientBlock from "../../ui/GradientBlock/GradientBlock";
 
 const FeedbackPage = () => {
   const form = useRef();
@@ -66,86 +67,86 @@ const FeedbackPage = () => {
   return (
     <Layout>
       <form ref={form} onSubmit={handleSubmit}>
-        <Flex gap={8} flexDirection={"column"} mx={"auto"} maxW={800}>
-          <Heading mt={5} textAlign={"center"}>
-            Feedback
-          </Heading>
-          <FormControl as="fieldset">
-            <FormLabel as="legend">
-              <Flex>
-                <Text fontSize={"xl"} color={"white"}>
-                  What do you want to tell
-                </Text>
-                <Text color={"red"}>*</Text>
-              </Flex>
-            </FormLabel>
-            <RadioGroup defaultValue={feedbackForm.mailType}>
-              <HStack spacing="24px">
-                <Radio
-                  name={"mailType"}
-                  onChange={onChangeValue}
-                  checked={feedbackForm.mailType === "Tell about the problem"}
-                  value="Tell about the problem"
-                >
-                  Tell about the problem
-                </Radio>
-                <Radio
-                  name={"mailType"}
-                  onChange={onChangeValue}
-                  checked={feedbackForm.mailType === "Suggest new features"}
-                  value="Suggest new features"
-                >
-                  Suggest new features
-                </Radio>
-              </HStack>
-            </RadioGroup>
-          </FormControl>
-          <FormControl isInvalid={feedbackFormErrors.email}>
-            <FormLabel>
-              <Flex>
-                <Text fontSize={"xl"} color={"white"}>
-                  Email
-                </Text>
-                <Text color={"red"}>*</Text>
-              </Flex>
-            </FormLabel>
-            <Input
-              type="email"
-              name={"email"}
-              value={feedbackForm.email}
-              onChange={handleInputEmailChange}
-              placeholder={"email"}
-            />
-            {!feedbackFormErrors.email ? (
-              <FormHelperText>Enter the email.</FormHelperText>
-            ) : (
-              <FormErrorMessage>Email is required.</FormErrorMessage>
-            )}
-          </FormControl>
-          <FormControl isInvalid={feedbackFormErrors.text}>
-            <FormLabel>
-              <Flex>
-                <Text fontSize={"xl"} color={"white"}>
-                  Text
-                </Text>
-                <Text color={"red"}>*</Text>
-              </Flex>
-            </FormLabel>
-            <Textarea
-              name={"text"}
-              type="text"
-              value={feedbackForm.text}
-              onChange={handleInputTextChange}
-              placeholder={"Text..."}
-            />
-            {!feedbackFormErrors.text ? (
-              <FormHelperText>Enter text.</FormHelperText>
-            ) : (
-              <FormErrorMessage>Text is required.</FormErrorMessage>
-            )}
-          </FormControl>
-          <Button type={"submit"}>Submit</Button>
-        </Flex>
+        <GradientBlock maxW={800} mt={5} mx={"auto"}>
+          <Flex gap={8} flexDirection={"column"}>
+            <Heading textAlign={"center"}>Feedback</Heading>
+            <FormControl as="fieldset">
+              <FormLabel as="legend">
+                <Flex>
+                  <Text fontSize={"xl"} color={"white"}>
+                    What do you want to tell
+                  </Text>
+                  <Text color={"red"}>*</Text>
+                </Flex>
+              </FormLabel>
+              <RadioGroup defaultValue={feedbackForm.mailType}>
+                <HStack spacing="24px">
+                  <Radio
+                    name={"mailType"}
+                    onChange={onChangeValue}
+                    checked={feedbackForm.mailType === "Tell about the problem"}
+                    value="Tell about the problem"
+                  >
+                    Tell about the problem
+                  </Radio>
+                  <Radio
+                    name={"mailType"}
+                    onChange={onChangeValue}
+                    checked={feedbackForm.mailType === "Suggest new features"}
+                    value="Suggest new features"
+                  >
+                    Suggest new features
+                  </Radio>
+                </HStack>
+              </RadioGroup>
+            </FormControl>
+            <FormControl isInvalid={feedbackFormErrors.email}>
+              <FormLabel>
+                <Flex>
+                  <Text fontSize={"xl"} color={"white"}>
+                    Email
+                  </Text>
+                  <Text color={"red"}>*</Text>
+                </Flex>
+              </FormLabel>
+              <Input
+                type="email"
+                name={"email"}
+                value={feedbackForm.email}
+                onChange={handleInputEmailChange}
+                placeholder={"email"}
+              />
+              {!feedbackFormErrors.email ? (
+                <FormHelperText>Enter the email.</FormHelperText>
+              ) : (
+                <FormErrorMessage>Email is required.</FormErrorMessage>
+              )}
+            </FormControl>
+            <FormControl isInvalid={feedbackFormErrors.text}>
+              <FormLabel>
+                <Flex>
+                  <Text fontSize={"xl"} color={"white"}>
+                    Text
+                  </Text>
+                  <Text color={"red"}>*</Text>
+                </Flex>
+              </FormLabel>
+              <Textarea
+                name={"text"}
+                type="text"
+                value={feedbackForm.text}
+                onChange={handleInputTextChange}
+                placeholder={"Text..."}
+              />
+              {!feedbackFormErrors.text ? (
+                <FormHelperText>Enter text.</FormHelperText>
+              ) : (
+                <FormErrorMessage>Text is required.</FormErrorMessage>
+              )}
+            </FormControl>
+            <Button type={"submit"}>Submit</Button>
+          </Flex>
+        </GradientBlock>
       </form>
     </Layout>
   );
